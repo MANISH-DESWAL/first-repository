636efc35b30222ffd6f103669c0087f7b5c8ed1b
MANISH DESWAL <manishdeswal0899@gmail.com>
2025-12-16 22:08:09 +0530

    added two files calculator and hello world

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+a="Hello World"
+k=int(input("Enter a num: -- "))
+for i in range(k):
+    b=1
+    print(f"{b} I'm printing {a}")
+    b+=1
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # first-repository
 its my 1st repo where ill learn about git.
+<br>
+Author - Manish Deswal
